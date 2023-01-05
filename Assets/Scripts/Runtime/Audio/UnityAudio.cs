c1fbd9fb0db94ed7848c7f3b060666aa31f55f19
Farid <Blackwazz7@gmail.com>
2023-01-05 19:21:00 +0500

    Music added

--- Assets/Scripts/Runtime/Audio/UnityAudio.cs
+++ Assets/Scripts/Runtime/Audio/UnityAudio.cs
@@ -30,26 +30,4 @@ namespace NotEnoughMemory.Audio
             _audioSource.PlayOneShot(_audioSource.clip);
         }
     }
-    
-    public sealed class UnityAudioWithRandomPlayingClip : MonoBehaviour, IUnityAudio
-    {
-        [SerializeField] private AudioSource _audioSource;
-        [SerializeField] private AudioClip[] _clips;
-
-        public bool IsPlaying => _audioSource.isPlaying;
-        
-        public void Play()
-        {
-       
-        }
-
-        public void Stop()
-        {
-    
-        }
-
-        public void PlayOneShot()
-        {
-        }
-    }
 }
---
+++ Assets/Scripts/Runtime/Audio/UnityAudioWithRandomPlayingClip.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace NotEnoughMemory.Audio
+{
+    public sealed class UnityAudioWithRandomPlayingClip : MonoBehaviour, IUnityAudio
+    {
+        [SerializeField] private AudioSource _audioSource;
+        [SerializeField] private AudioClip[] _clips;
+
+        public bool IsPlaying => _audioSource.isPlaying;
+        
+        public void Play()
+        {
+       
+        }
+
+        public void Stop()
+        {
+    
+        }
+
+        public void PlayOneShot()
+        {
+            var randomClip = _clips[Random.Range(0, _clips.Length)];
+            _audioSource.PlayOneShot(randomClip);
+        }
+    }
+}
